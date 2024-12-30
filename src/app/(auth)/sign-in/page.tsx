737532e104bd86eb91abe0a521842b3ca809f05a
Md-Kutub-Islam <mdkutubislam09@gmail.com>
2024-12-30 14:15:09 +0530

    sign in page and functionallity complete

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,149 @@
-import React from "react";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useDebounceCallback } from "usehooks-ts";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schema/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { signInSchema } from "@/schema/signInSchema";
+import { signIn } from "next-auth/react";
 
 function page() {
-  return <div>page</div>;
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { toast } = useToast();
+  const router = useRouter();
+
+  // zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    // for resolver we use zod Resolver and it schema
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setIsSubmitting(false);
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier, //this is autometically access email or username and in our code identifier is email or username because we use email or username identifier for next auth
+      password: data.password,
+    });
+
+    if (result?.error) {
+      // in case something wrong in credentials then if block code run
+      if (result.error == "CredentialsSignin") {
+        toast({
+          title: "Login Failed",
+          description: "Incorrect email or password",
+          variant: "destructive",
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: result.error,
+          variant: "destructive",
+        });
+      }
+    }
+
+    if (result?.url) {
+      console.log("sign-in:", result); // i have to check what it is return
+      setIsSubmitting(true);
+      router.replace("/dashboard");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">Sign in to start your anohymous adventure</p>
+        </div>
+
+        {/* form wrok */}
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="email/username"
+                      {...field}
+                      // here is we not use onchange because it is handle it autometically but we use onChange in username because we have to check username is exist or not
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="password"
+                      {...field}
+                      // here is we not use onchange because it is handle it autometically but we use onChange in username because we have to check username is exist or not
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                "Signin"
+              )}
+            </Button>
+          </form>
+        </Form>
+
+        <div className=" text-center mt-4">
+          <p>
+            Don't have an account?{""}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default page;
